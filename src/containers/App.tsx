6f93c74594371a737a9c63a56e36a779a45cac4c
moshe0 <qwmoshe@gmail.com>
2018-08-23 19:19:42 +0300

    ..

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,65 +1,146 @@
 import * as React from 'react';
-// import * as fs from "fs";
 
+
+interface Iimage {
+    id : number,
+    src : string
+}
 
 interface IAppState {
-    srcArr : string[]
+    srcArr : Iimage[]
 }
 
 class App extends React.Component<{}, IAppState> {
+    db : any;
     constructor(props: {}) {
         super(props);
 
         this.state = {
             srcArr: []
         };
-
     }
-
-    /*
-    readFile() {
-        return fs.readFileSync(`${__dirname}\\ArrSrc.json`).toString();
-    }
-
-
-    writeFile(){
-        fs.writeFile(`${__dirname}\\ArrSrc.json`, JSON.stringify(this.state.srcArr), function(err) {
-            if (!!err)
-                return "failed";
-            else
-                return "succeeded";
-        });
-
-    }
-    */
 
     onAdd = (e: React.ChangeEvent<HTMLInputElement>) =>{
         let file = e.target.files;
         if (file) {
             const fr = new FileReader();
             fr.onload = (data) => {
-                let arr = this.state.srcArr;
-                arr.push(fr.result);
-                this.setState({
-                    srcArr: arr
-                });
+                this.AddToIndexDB(fr.result);
             };
             fr.readAsDataURL(file[0]);
         }
     };
 
 
+    AddToIndexDB = (result : any) =>{
+        let tran = this.db.transaction(["Images"], "readwrite");
+
+        tran.oncomplete = function() {
+            console.log("tran oncomplete");
+        };
+
+        tran.onabort = function() {
+            console.log("tran onabort")
+        };
+
+        tran.onerror = function() {
+            console.log("tran onerror")
+        };
+
+        const images = tran.objectStore("Images");
+        const request = images.add({src: result});
+
+        request.onsuccess = (e : any) =>{
+            console.log("add success", e);
+
+            let aaa = this.state.srcArr;
+            aaa.push({id: e.target.result ,src: result});
+
+            this.setState({
+                srcArr: aaa
+            });
+        };
+
+        request.onerror = (e : any) =>{
+            console.log("add error", e);
+        };
+    };
+
+
+    initIndexDB = () =>{
+        const request = indexedDB.open("ImagesDB", 1);
+
+        request.onsuccess = (e : any) =>{
+            console.log("DB opened successfully");
+            this.db = e.target.result;
+            this.getImages();
+        };
+
+        request.onupgradeneeded = (e : any) =>{
+            console.log("upgradeneeded", e);
+            const dbdd = e.target.result;
+            dbdd.createObjectStore("Images", {keyPath: "id", autoIncrement: true});
+        };
+
+        request.onerror = (err : any) =>{
+            console.error(err);
+        };
+    };
+
+    getImages = () =>{
+        let tran = this.db.transaction(["Images"], "readonly");
+
+        tran.oncomplete = function() {
+            console.log("tran oncomplete")
+        };
+
+        tran.onabort = function() {
+            console.log("tran onabort")
+        };
+
+        tran.onerror = function() {
+            console.log("tran onerror")
+        };
+
+        const images = tran.objectStore("Images");
+        const request = images.openCursor();
+        let storeImages : Iimage[] = [];
+
+        request.onsuccess = (e : any) =>{
+            console.log("add success", e);
+            let cursor = e.target.result;
+            if (cursor) {
+                storeImages.push(cursor.value);
+                cursor.continue();
+            }
+            else{
+                console.log("Done Reading!");
+                this.setState({
+                    srcArr: storeImages
+                });
+            }
+        };
+
+        request.onerror = (e : any) =>{
+            console.log("add error", e);
+        }
+    };
+
+    componentWillMount(){
+        this.initIndexDB();
+    }
+
+
     public render() {
         console.log(this.state.srcArr);
         const listSrc = this.state.srcArr.map((item, idx) => {
-            return <img key={idx} className="image" src={item}/>
+            return <img key={idx} className="image" src={item.src}/>
         });
 
 
         return (
             <div className="Images">
-                <label htmlFor="file-input" className="input-label">
-                    Add image
+                <label htmlFor="file-input" className="input-label-add">
                     <input type="file" id="file-input" accept="image/png, image/jpeg" onChange={this.onAdd}/>
                 </label>
                 {listSrc}
